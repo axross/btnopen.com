8065200bcfd17fef2f112298ab415da1d57f667f
Kohei Asai <yo@axross.dev>
2026-02-21 17:10:16 -0800

    test commint

--- src/services/graphql.ts
+++ src/services/graphql.ts
@@ -16,6 +16,9 @@ export const { getClient, query, PreloadQuery } = registerApolloClient(() => {
 				// you can pass additional options that should be passed to `fetch` here,
 				// e.g. Next.js-related `fetch` options regarding caching and revalidation
 				// see https://nextjs.org/docs/app/api-reference/functions/fetch#fetchurl-options
+				headers: {
+					authorization: `Bearer ${process.env.HASHNODE_TOKEN}`,
+				},
 			},
 		}),
 	});
