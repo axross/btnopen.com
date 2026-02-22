a6f777159102bce29c54e09594fcce533f248ee0
Kohei Asai <yo@axross.dev>
2026-02-22 11:15:17 -0800

    chore: delete old graphql code

--- src/services/graphql.ts
+++ src/services/graphql.ts
@@ -7,41 +7,15 @@ import {
 
 const hashnodeGraphqlEndpoint = "https://gql.hashnode.com";
 
-export const { getClient, query, PreloadQuery } = registerApolloClient(() => {
-	return new ApolloClient({
-		cache: new InMemoryCache(),
-		link: new HttpLink({
-			uri: hashnodeGraphqlEndpoint,
-			fetchOptions: {
-				// you can pass additional options that should be passed to `fetch` here,
-				// e.g. Next.js-related `fetch` options regarding caching and revalidation
-				// see https://nextjs.org/docs/app/api-reference/functions/fetch#fetchurl-options
-			},
+export const { getClient, query, PreloadQuery } = registerApolloClient(
+	() =>
+		new ApolloClient({
+			cache: new InMemoryCache(),
+			link: new HttpLink({
+				uri: hashnodeGraphqlEndpoint,
+			}),
 		}),
-	});
-});
-
-// interface HashnodeGraphQlErrorExtensions {
-// 	field: string;
-// 	context: unknown[];
-// 	code: string;
-// }
-
-// export async function execute<Result, Variables>(
-// 	query: TypedDocumentString<Result, Variables>,
-// 	...[variables]: Variables extends Record<string, never> ? [] : [Variables]
-// ): Promise<FormattedExecutionResult<Result, HashnodeGraphQlErrorExtensions>> {
-// 	const response = await fetch("https://gql.hashnode.com", {
-// 		method: "POST",
-// 		headers: {
-// 			"content-type": "application/json",
-// 			accept: "application/graphql-response+json",
-// 		},
-// 		body: JSON.stringify({ query, variables }),
-// 	});
-
-// 	return await response.json();
-// }
+);
 
 // biome-ignore lint/performance/noBarrelFile: generated file; cannot change the structure to avoid barrel file
 export { gql } from "./graphql/generated";
